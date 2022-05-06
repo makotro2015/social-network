d0e86daec1f4af71fb251ac7c9677c4da426d0fc
makotro2015 <makotro2015@gmail.com>
2022-05-06 18:12:58 +0700

    Updates

---
+++ precache-manifest.8e84b7bc19cf9ce5ef07e14746787b40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbac8b2c10543e55b255f787541d8eee",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "8423ae4e3504a8a11be5",
+    "url": "/social-network/static/css/main.e56f2435.chunk.css"
+  },
+  {
+    "revision": "1f14373d11511134deee",
+    "url": "/social-network/static/js/2.5734e71c.chunk.js"
+  },
+  {
+    "revision": "8423ae4e3504a8a11be5",
+    "url": "/social-network/static/js/main.d0191283.chunk.js"
+  },
+  {
+    "revision": "ac3bf8d38c497c047824",
+    "url": "/social-network/static/js/runtime~main.a3e35632.js"
+  },
+  {
+    "revision": "d1f059aa7be8c7ebc9b900b81c3e998a",
+    "url": "/social-network/static/media/loading.d1f059aa.svg"
+  }
+]);
